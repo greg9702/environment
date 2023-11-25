a7b9accdd5adcb830d03861549ae7037d7d5cdf3
greg9702 <grzegorzb9702@gmail.com>
2023-11-25 10:10:57 +0100

    Removed buffer and text snippets

--- config/nvim/lua/thdxr/packer.lua
+++ config/nvim/lua/thdxr/packer.lua
@@ -18,9 +18,6 @@ return require('packer').startup(function()
 
 		  -- Autocompletion
 		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
 		  {'hrsh7th/cmp-nvim-lsp'},
 		  {'hrsh7th/cmp-nvim-lua'},
 
